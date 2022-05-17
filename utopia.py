46730cec65c9f81893a191d1e50bd81fe19d0a4d
Astramast <azerqsdfazerqsdg@gmail.com>
2022-05-17 23:33:17 +0200

    Tiens mael

---
+++ utopia.py
@@ -0,0 +1,20 @@
+def f(string):
+    try:
+        s = int(input(string))
+    except:
+        print("Mauvais input, veuillez réessayer")
+        f(string)
+    return s
+
+k = ["Rang de l'attaqué : ", "Rang de l'attaquant : ", "Bonus/Malus : "]
+l=[]
+u = 1
+while u = 1:
+    print("Calcul de l'objectif : ")
+    print("50 + (? - ?) * 5 + ?")
+    for i in k:
+        l.append(f(i))
+    print(f"50 + ({l[0]} - {l[1]}) * 5 + l[2]")
+    print("Objectif =", 50+(l[0] - l[1]) *5 + l[2])
+    u = f("Continuer ? (Écrire 1 si oui 0 si non) : ")
+
